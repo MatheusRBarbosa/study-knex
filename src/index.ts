6964272c3c8fcf34e5f59fb70883b0100127161e
Matheus Barbosa <matheuskleber@live.com>
2023-06-22 09:53:25 -0300

    prepare benchmark

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 const main = async () => {
-    console.log("hello world")
-}
+  console.log(">> Insert object 1");
+  console.log(">> Insert object 2");
+
+  console.log(">> Insert User");
+  console.log(">> Insert Profile");
+  console.log(">> Get User");
+  console.log(">> Update User | Attach profile");
+
+  console.log(">> Attach object 1 and 2 to User");
+  console.log(">> Return User with all relationships");
+};
 
 main();
